9b9b88c19004af14b0f74a56df1c9b04852748b7
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-03-01 15:53:36 -0500

    Add routes to new components and add classnames

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddOns from './components/AddOns';
+import SelectPlan from './components/SelectPlan';
+import SideNav from './components/SideNav';
+import Summary from './components/Summary';
+import YourInfo from './components/YourInfo';
 
 function App() {
   return (
-    <div>
-      test
+    <div className='overall-app'>
+      <SideNav />
+      <Routes>
+        <Route path='/' element={<YourInfo />} />
+        <Route path='/selectplan' element={<SelectPlan />} />
+        <Route path='addons' element={<AddOns />} />
+        <Route path='summary' element={<Summary />} />
+      </Routes>
     </div>
   );
 }
