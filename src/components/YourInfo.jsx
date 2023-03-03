f4800baf4f683a13eb80433d74ae070741e9d100
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-03-03 14:26:38 -0500

    Modify tags for better styling

--- src/components/YourInfo.jsx
+++ src/components/YourInfo.jsx
@@ -16,19 +16,21 @@ const YourInfo = () => {
 
   return (
     <div className='all-your-info'>
-      <div className='personal'>Personal info</div>
-      <div className='request'>Please provide your name, email address and phone number.</div>
-      <div className='info-input-title'>
-        <label className='info-title'>Name</label>
-        <input className='info-input' type='text' placeholder='e.g. Stephen King' />
-      </div>
-      <div className='info-input-title'>
-        <label className='info-title'>Email Address</label>
-        <input className='info-input' type='email' placeholder='e.g. stephenking@email.com' />
-      </div>
-      <div className='info-input-title'>
-        <label className='info-title'>Phone Number</label>
-        <input className='info-input' type='number' placeholder='e.g. +1 123 456 7890' />
+      <div>
+        <div className='personal'>Personal info</div>
+        <div className='request'>Please provide your name, email address and phone number.</div>
+        <div className='info-input-title'>
+          <label className='info-title'>Name</label>
+          <input className='info-input' type='text' placeholder='e.g. Stephen King' />
+        </div>
+        <div className='info-input-title'>
+          <label className='info-title'>Email Address</label>
+          <input className='info-input' type='email' placeholder='e.g. stephenking@email.com' />
+        </div>
+        <div className='info-input-title'>
+          <label className='info-title'>Phone Number</label>
+          <input className='info-input' type='number' placeholder='e.g. +1 123 456 7890' />
+        </div>
       </div>
 
       <button onClick={handleNextButton} className='next-step'>Next Step</button>
