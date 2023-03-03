1a88bde2ade7644f36ff8673e9c3bddccd938650
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-03-02 19:29:37 -0500

    Add states and useContext to handle them

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+
+import GeneralContext from './contexts/GeneralContext';
 import AddOns from './components/AddOns';
 import SelectPlan from './components/SelectPlan';
 import SideNav from './components/SideNav';
 import Summary from './components/Summary';
 import YourInfo from './components/YourInfo';
 
+import './App.css';
+
 function App() {
+  const [yourInfo, setYourInfo] = useState(true);
+  const [plan, setPlan] = useState(false);
+  const [addOns, setAddOns] = useState(false);
+  const [summary, setSummary] = useState(false);
+
   return (
-    <div className='overall-app'>
-      <SideNav />
-      <Routes>
-        <Route path='/' element={<YourInfo />} />
-        <Route path='/selectplan' element={<SelectPlan />} />
-        <Route path='addons' element={<AddOns />} />
-        <Route path='summary' element={<Summary />} />
-      </Routes>
-    </div>
+    <GeneralContext.Provider value={{ yourInfo, setYourInfo, plan, setPlan, addOns, setAddOns, summary, setSummary }}>
+      <div className='overall-app'>
+        <SideNav />
+        <Routes>
+          <Route path='/' element={<YourInfo />} />
+          <Route path='/selectplan' element={<SelectPlan />} />
+          <Route path='/addons' element={<AddOns />} />
+          <Route path='/summary' element={<Summary />} />
+        </Routes>
+      </div>
+    </GeneralContext.Provider>
   );
 }
 
