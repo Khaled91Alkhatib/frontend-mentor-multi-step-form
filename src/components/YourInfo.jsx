3f1588d120d097775d26019fb3485473032cf3c5
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-03-14 14:17:09 -0400

    Fix a bug in input values and access it based on new state declaration

--- src/components/YourInfo.jsx
+++ src/components/YourInfo.jsx
@@ -5,13 +5,16 @@ import GeneralContext from '../contexts/GeneralContext';
 import '../styles/YourInfo.scss';
 
 const YourInfo = () => {
-  const { setYourInfo, setPlan, userInputs, setUserInputs } = useContext(GeneralContext);
+  const { userInputs, setUserInputs } = useContext(GeneralContext);
+  console.log(userInputs, "yourinfo");
   const navigate = useNavigate();
 
   const handleNextButton = () => {
     navigate('/selectplan');
-    setPlan(true);
-    setYourInfo(false);
+    setUserInputs(prev => ({
+      yourInfo: !prev.yourInfo,
+      plan: !prev.plan
+    }));
   };
 
   const handleInputChange = (event) => {
@@ -26,15 +29,15 @@ const YourInfo = () => {
         <div className='request'>Please provide your name, email address and phone number.</div>
         <div className='info-input-title'>
           <label className='info-title'>Name</label>
-          <input name='name' value={userInputs.name} onChange={handleInputChange} className='info-input' type='text' placeholder='e.g. Stephen King' />
+          <input name='name' value={userInputs.name ?? ''} onChange={handleInputChange} className='info-input' type='text' placeholder='e.g. Stephen King' />
         </div>
         <div className='info-input-title'>
           <label className='info-title'>Email Address</label>
-          <input name='email' value={userInputs.email} onChange={handleInputChange} className='info-input' type='email' placeholder='e.g. stephenking@email.com' />
+          <input name='email' value={userInputs.email ?? ''} onChange={handleInputChange} className='info-input' type='email' placeholder='e.g. stephenking@email.com' />
         </div>
         <div className='info-input-title'>
           <label className='info-title'>Phone Number</label>
-          <input name='phone' value={userInputs.phone} onChange={handleInputChange} className='info-input' type='number' placeholder='e.g. +1 123 456 7890' />
+          <input name='phone' value={userInputs.phone ?? ''} onChange={handleInputChange} className='info-input' type='number' placeholder='e.g. +1 123 456 7890' />
         </div>
       </div>
 
