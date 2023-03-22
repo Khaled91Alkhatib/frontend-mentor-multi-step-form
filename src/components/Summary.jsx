66c7b7dde8cb739e0fce57289f2dbae68444607b
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-03-22 14:57:43 -0400

    Add more details to the summary page based on previous selections

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -5,8 +5,9 @@ import GeneralContext from '../contexts/GeneralContext';
 import "../styles/Summary.scss";
 
 const Summary = () => {
-  const { selectedPlan, monthly, setUserInputs } = useContext(GeneralContext);
-  console.log(selectedPlan);
+  const { selectedPlan, monthly, setUserInputs, allAddOns } = useContext(GeneralContext);
+  // console.log(selectedPlan);
+  console.log(allAddOns);
 
   const navigate = useNavigate();
 
@@ -28,6 +29,16 @@ const Summary = () => {
     }));
   };
 
+  const selectedItems = allAddOns.filter(addOn => addOn.isChecked);
+  console.log(selectedItems);
+
+  const totalPrice = selectedItems.reduce((total, selectedItem) => {
+    return total + (monthly ? selectedItem.price : selectedItem.price * 10);
+  }, selectedPlan.price);
+
+  const handleConfirm = () => {
+    navigate('/thankyou')
+  }
 
   return (
     <div className='every-main-page'>
@@ -40,19 +51,37 @@ const Summary = () => {
       <div>
 
         <div className='plan-addons'>
-          <div className='plan-addon-price'>
-            <div className='selected-plan'>{selectedPlan.name} {monthly ? "(Monthly)" : "(Yearly)"}</div>
-            <div className='selected-price'>${selectedPlan.price}{monthly ? "/mo" : "/yr"}</div>
+
+          <div className='inner-details'>
+            <div className='plan-addon-price'>
+              <div className='selected-plan'>{selectedPlan.name} {monthly ? "(Monthly)" : "(Yearly)"}</div>
+              <div className='selected-price'>${selectedPlan.price}{monthly ? "/mo" : "/yr"}</div>
+            </div>
+            <button onClick={handleChangeButton} className='change-button'>Change</button>
+            <div className='vertical-line'></div>
+            {selectedItems.length > 0 ?
+              selectedItems.map(selectedItem => (
+                <div className='addons' key={selectedItem.id}>
+                  <div>{selectedItem.title}</div>
+                  <div className='selected-addon-price'>+${monthly ? selectedItem.price : selectedItem.price * 10}{monthly ? "/mo" : "/yr"}</div>
+                </div>
+              ))
+              :
+              <div className='addons'>No Addons selected!</div>
+            }
           </div>
-          <button onClick={handleChangeButton} className='change-button'>Change</button>
-          <div className='vertical-line'></div>
+
+          <div className='total-price'>
+            <div>Total {monthly ? "(per month)" : "(per year)"}</div>
+            <div className='total-price-amount'>+${totalPrice}{monthly ? "/mo" : "/yr"}</div>
+          </div>
         </div>
 
       </div>
 
       <div className='back-next-buttons'>
         <button onClick={handleBackButton} className='back-button'>Go Back</button>
-        <button className='next-step'>Confirm</button>
+        <button onClick={handleConfirm} className='next-step' style={{backgroundColor:'blue'}}>Confirm</button>
       </div>
 
     </div>
