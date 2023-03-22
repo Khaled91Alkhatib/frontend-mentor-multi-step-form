2eda61479d728dca6b5b4bbefbdd65fdad0821dd
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-03-22 14:58:42 -0400

    Create new page to show after user clicks confirm on the summary page

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import thankYouImage from '../assests/images/icon-thank-you.svg';
+import '../styles/ThankYou.scss';
 
 const ThankYou = () => {
   return (
-    <div>ThankYou</div>
-  )
-}
 
-export default ThankYou
+
+    <div className='every-main-page' style={{ textAlign: 'center' }}>
+      <img className='thank-you-image' src={thankYouImage} alt='thankyou' />
+      <div className='personal'>Thank you!</div>
+      <div className='request'>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</div>
+    </div>
+  );
+};
+
+export default ThankYou;
