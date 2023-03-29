47de84b1646cc6615e9e4a25a4e9543b657769c7
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-03-29 13:26:48 -0400

    Add a button to return to main 'info' page

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import thankYouImage from '../assests/images/icon-thank-you.svg';
+import GeneralContext from '../contexts/GeneralContext';
 import '../styles/ThankYou.scss';
 
 const ThankYou = () => {
+  const navigate = useNavigate();
+  const { setUserInputs, setMonthly, setYearly, setSelectedPlan } = useContext(GeneralContext);
+
+  const handleMainPage = () => {
+
+    navigate('/');
+    setUserInputs({
+      yourInfo: true,
+      plan: false,
+      addOns: false,
+      summary: false,
+      name: "",
+      email: "",
+      phone: "",
+    });
+    setMonthly(true);
+    setYearly(false);
+    setSelectedPlan({});
+
+  };
+
   return (
 
 
@@ -11,6 +34,7 @@ const ThankYou = () => {
       <img className='thank-you-image' src={thankYouImage} alt='thankyou' />
       <div className='personal'>Thank you!</div>
       <div className='request'>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</div>
+      <button className='main-page-button' onClick={handleMainPage}>Main Page</button>
     </div>
   );
 };
